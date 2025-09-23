fbd0293f5f7ebbbbbc4b5b6a9b3f0b19910076e7
Enamul57 <32171261+Enamul57@users.noreply.github.com>
2025-09-23 17:23:43 +0600

    Update User.php
    check user has modules through role

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+        public function authUserHasModule($slug)
+    {
+        return auth()->user()->roles()->with('features')->get()->pluck('features.*.slug')->flatten()->contains($slug);
+    }
+
+    public function userHasModule($slug)
+    {
+        return $this->roles()->with('features')->get()->pluck('features.*.slug')->flatten()->contains($slug);
+    }
 }
